2ac8ee83b5b7ca3b411271676e5781f304d0be6f
Patrícia Cavalcante <patriciacavalcante@gmail.com>
2022-09-01 16:58:24 -0300

    Adicionar arquivos de projeto.

---
+++ Bank/Contacorrente.cs
@@ -0,0 +1,13 @@
+﻿
+namespace bytebank
+{
+    class ContaCorrente
+    {
+       public string titular;
+       public string conta;
+       public string agencia;
+       public int numero_agencia;
+       public string nome_agencia;
+       public double saldo;
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,20 @@
+﻿using bytebank; //chamou a classe bytebank
+
+Console.WriteLine("Bem-vindo ao Bytebank!"); 
+
+
+ContaCorrente conta1 = new ContaCorrente(); //criou o objeto 
+
+conta1.titular = "Marcella Pereira"; //acessa a prorpeidade com .
+conta1.conta = "10123-X";
+conta1.numero_agencia = 23;
+conta1.nome_agencia = "Ag~encia Central";
+conta1.saldo = 100;
+
+Console.WriteLine("Titular: " + conta1.titular); //usa . para acessar e printar o valor na tela
+Console.WriteLine("Conta: " + conta1.conta);
+Console.WriteLine("Número Agencia: " + conta1.numero_agencia);
+Console.WriteLine("Nome da Agencia: " + conta1.nome_agencia);
+Console.WriteLine("Saldo: " + conta1.saldo);
+
+Console.ReadKey();
